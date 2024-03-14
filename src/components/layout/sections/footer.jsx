6acd1f7e09ae14adcebf8cbedecc0d109020030f
Franco Bulacio <francobulacio@gmail.com>
2024-03-14 11:24:45 -0300

    Billetera React

--- src/components/layout/sections/footer.jsx
+++ src/components/layout/sections/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex w-full fixed items-center justify-center bottom-0 bg-sky-50">
       <img src={alkemyLogo} className="logo" alt="logo" />
-      <h6 className="text-sm">©2022 | Made by ResilientDevs™</h6>
+      <h6 className="text-sm">©2022 | Made by Franco Bulacio</h6>
     </footer>
   );
 };
